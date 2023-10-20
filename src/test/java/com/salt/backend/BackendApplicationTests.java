602d4b01f867a8cddb4e084b4952bece75654f91
Wilvan <wilvan@aneldi.com>
2023-10-20 14:19:32 +0700

    init commit

---
+++ src/test/java/com/salt/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
